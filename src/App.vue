ef5d7a483ea8b16a3d2cf5d938d27426f5991251
mcldays <mcldays@gmail.com>
2021-08-25 18:38:01 +0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app>
+
+  </v-app>
+</template>
+
+<script lang="ts">
+
+import HelloWorld from './components/HelloWorld.vue';
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component({
+  components:{
+    HelloWorld
+  }
+})
+export default class App extends Vue {}
+</script>

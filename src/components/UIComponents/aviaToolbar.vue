be201a3be9bcd071420ca81a964937a70370dd8a
mcldays <mcldays@gmail.com>
2021-11-05 20:34:19 +0300

    Update aviaToolbar.vue

--- src/components/UIComponents/aviaToolbar.vue
+++ src/components/UIComponents/aviaToolbar.vue
@@ -2,15 +2,9 @@
 
 </template>
 
-<script lang="ts">
-import {Component, Vue} from "vue-property-decorator";
-@Component({
-  components:{
-
-  }
-})
-export default class AviaToolbar extends Vue {
- private tab : any
+<script>
+export default {
+  name: "aviaToolbar"
 }
 </script>
 

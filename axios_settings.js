79db1e6df34cb91360029b6cb5ef0b2d0d966352
Нурислам Гиззатуллин <gizz.nur@gmail.com>
2021-12-01 13:30:58 +0400

    Подключение

--- axios_settings.js
+++ axios_settings.js
@@ -2,9 +2,9 @@ var axios = require('axios');
 
 var axiosInstance = axios.create({
     // baseURL: "http://89.108.103.251:8080",
-    baseURL: "https://localhost:5001",
+    //baseURL: "https://localhost:5001",
     // baseURL: "http://89.108.77.147:8080",
-     //baseURL: "https://api.cargo-rmgroup.ru/",
+    baseURL: "https://api.cargo-rmgroup.ru/",
     /* other custom settings */
 });
 
